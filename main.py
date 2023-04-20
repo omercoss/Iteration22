97d445a5708942dee8680989517c425e436e6432
omercoss <127502496+omercoss@users.noreply.github.com>
2023-04-20 18:09:08 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication
-from gui.multielectronicjournalstart1 import StartSide
-
-if __name__ == "__main__":
-    app = QApplication([])
-    start_side = StartSide
-    start_side.show()
-    app.exec()
-
